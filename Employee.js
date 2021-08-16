5cac770ce32b91aa0111323bb03e1cfccfb02186
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-16 22:24:33 +0530

    [Afrath]updated the file with validating id,gender,startDate

--- Employee.js
+++ Employee.js
@@ -7,13 +7,23 @@ class EmployeePayrollData {
   startDate;
   //constructor to store data
   constructor(...params) {
-    this.id = params[0];
+      //validates Id for non zero positive number
+    let idRegex = RegExp("^[1-9][0-9]{0,}$");
+    if (idRegex.test(params[0])) this.id = params[0];
+    else throw "Invalid Id";
     let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
     if (nameRegex.test(params[1])) this.name = params[1];
     else throw "Invalid name";
     this.salary = params[2];
+    //validate m or f 
+    let genderRegex = RegExp("^[M|F]$");
+    if(genderRegex.test(params[3]))
     this.gender = params[3];
+    else throw 'Invalid Gender';
+    //date validation
+    if(params[4]<=new Date().toLocaleDateString())
     this.startDate = params[4];
+    else throw 'Invalid date';
   }
   get name() {
     return this._name;
@@ -55,6 +65,7 @@ try {
 } catch (e) {
   console.error(e);
 }
+date = new Date('2021-07-16');
 try {
   employee = new EmployeePayrollData(
     1,
